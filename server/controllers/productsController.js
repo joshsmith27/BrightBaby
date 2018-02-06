651e41ee409395beb11b183c78ef84846931a250
Joshua Richard Smith <veryjoshua@gmail.com>
2018-02-05 22:55:53 -0700

    finished schema

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -19,4 +19,22 @@ module.exports = {
 			});
         }
     },
+    getProducts: ( req, res, next ) => {
+        const dbInstance = dbGetter(req);
+        if (req.user.id){
+            dbInstance.get_products([0])
+			.then(products => { res.status(200).send(products); })
+			.catch( err => {
+				console.log(err);
+				res.status(500).send(err);
+			});
+        }else{
+            dbInstance.get_products([1])
+			.then(products => { res.status(200).send(products); })
+			.catch( err => {
+				console.log(err);
+				res.status(500).send(err);
+			});
+        }
+    },
 }
