1aa9090857d0939a4bfdcca9ee25c1bdacebeaae
Joshua Richard Smith <veryjoshua@gmail.com>
2018-03-01 23:22:21 -0700

    finished kata

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import Loader from '../loading'
-import images from '../../images/img_0.png'
 class Home extends Component {
 	render() {
-
 		return (
 			<div className="Home-Container">
 				<Loader/>
